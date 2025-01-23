ca0df9a2ce0da78403bc158a542e88327ec86f58
afonsomm <afonso@matosomagalhaes.com>
2025-01-23 22:26:49 +0000

    useless commit

--- install/terminal/app-git-credential-manager.sh
+++ install/terminal/app-git-credential-manager.sh
@@ -1,4 +1,4 @@
-# Git credential helper that supports major source control hosts.
+# Git credential helper that supports major source control hosts
 cd /tmp
 wget -O gcm.tar.gz "https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.1/gcm-linux_amd64.2.6.1.tar.gz"
 sudo tar -xvf ./gcm.tar.gz -C /usr/local/bin
