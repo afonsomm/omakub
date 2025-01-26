656235b82acb2989ec1a3018bef439d805416f74
afonsomm <afonso@matosomagalhaes.com>
2025-01-26 18:53:58 +0000

    add Reaper DAW

---
+++ applications/Reaper.sh
@@ -0,0 +1,12 @@
+cat <<EOF >$HOME/.local/share/applications/Reaper.desktop
+[Desktop Entry]
+Version=1.0
+Name=Reaper
+Comment=Digital Audio Workstation
+Exec=$HOME/.local/share/REAPER/reaper
+Terminal=false
+Type=Application
+Icon=$HOME/.local/share/omakub/applications/icons/Reaper.png
+Categories=DAW;
+StartupNotify=false
+EOF
---
+++ uninstall/app-reaper.sh
@@ -0,0 +1,4 @@
+sudo rm -rf \
+	$HOME/.local/share/applications/Reaper.desktop \
+	$HOME/.local/share/REAPER \
+	$HOME/.config/REAPER
