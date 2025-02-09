516e13340f2afb00eaab3aa1510154df090729ea
afonsomm <afonso@matosomagalhaes.com>
2025-02-09 23:35:58 +0000

    change reaper version

--- install/desktop/app-reaper.sh
+++ install/desktop/app-reaper.sh
@@ -1,5 +1,5 @@
 cd /tmp
-wget -O reaper.tar.xz "https://www.reaper.fm/files/7.x/reaper730_linux_x86_64.tar.xz"
+wget -O reaper.tar.xz "https://www.reaper.fm/files/7.x/reaper733_linux_x86_64.tar.xz"
 sudo tar -xvf reaper.tar.xz -C .
 mv reaper_linux_x86_64/REAPER $HOME/.local/share
 rm -rf reaper.tar.xz reaper_linux_x86_64
