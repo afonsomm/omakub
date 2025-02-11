7eb2953c7670f3b3b2388fa5c36abf2482e8fa59
afonsomm <afonso@matosomagalhaes.com>
2025-02-11 13:49:35 +0000

    correct plugdata install

--- install/desktop/app-plugdata.sh
+++ install/desktop/app-plugdata.sh
@@ -1,4 +1,4 @@
 echo 'deb http://download.opensuse.org/repositories/home:/plugdata/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:plugdata.list
-curl -fsSL https://download.opensuse.org/repositories/home:plugdata/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_plugdata.gpg > /dev/null
+curl -fsSL https://download.opensuse.org/repositories/home:plugdata/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_plugdata.gpg >/dev/null
 sudo apt update
-sudo apt install plugdata
+sudo apt install plugdata -y
